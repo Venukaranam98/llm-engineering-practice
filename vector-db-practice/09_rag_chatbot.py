b2dfeb213344d717b9c1b66fc63e53a42ac8f9dc
venukaranam98 <vk3344556677@gmail.com>
2026-06-27 16:50:49 +0530

    feat: build interactive RAG chatbot with LangChain and ChromaDB

---
+++ vector-db-practice/09_rag_chatbot.py
@@ -0,0 +1,68 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = Chroma(
+    persist_directory="./chroma_db",
+    collection_name="metadata_demo",
+    embedding_function=embedding_model,
+)
+
+retriever = vectorstore.as_retriever(
+    search_kwargs={"k": 2}
+)
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+prompt = ChatPromptTemplate.from_template(
+    """
+You are a helpful AI assistant.
+
+Answer ONLY using the context below.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+)
+
+chain = prompt | llm
+
+print("RAG Chatbot Started! Type 'exit' to quit.\n")
+
+while True:
+    question = input("You: ")
+
+    if question.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    docs = retriever.invoke(question)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    response = chain.invoke(
+        {
+            "context": context,
+            "question": question
+        }
+    )
+
+    print("\nAI:", response.content)
+    print("-" * 50)
